d5c75d37e697dd8254cdbd2daadfe3efa3523e24
Igor Zhaglo <jagloik@gmail.com>
2026-05-29 00:11:34 +0300

    Configure admin for users and projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'owner',
+        'status',
+        'created_at',
+        'participants_count',
+    )
+    list_filter = (
+        'status',
+        'created_at',
+    )
+    search_fields = (
+        'name',
+        'description',
+        'owner__email',
+        'owner__name',
+        'owner_surname',
+    )
+    readonly_fields = (
+        'created_at',
+    )
+    filter_horizontal = (
+        'participants',
+    )
+
+    def participants_count(self, obj):
+        return obj.participants.count()
+
+    participants_count.short_description = 'Участников'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,100 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ('email',)
+    list_display = (
+        'id',
+        'email',
+        'name',
+        'surname',
+        'phone',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+    )
+    list_filter = (
+        'is_active',
+        'is_staff',
+        'is_superuser',
+    )
+    search_fields = (
+        'email',
+        'name',
+        'surname',
+        'phone',
+    )
+    filter_horizontal = (
+        'groups',
+        'user_permissions',
+        'favorites',
+    )
+
+    fieldsets = (
+        (
+            'Данные для входа',
+            {
+                'fields': (
+                    'email',
+                    'password',
+                ),
+            },
+        ),
+        (
+            'Личная информация',
+            {
+                'fields': (
+                    'name',
+                    'surname',
+                    'avatar',
+                    'phone',
+                    'github_url',
+                    'about',
+                    'favorites',
+                ),
+            },
+        ),
+        (
+            'Права доступа',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (
+            'Важные даты',
+            {
+                'fields': (
+                    'last_login',
+                ),
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            'Создание пользователя',
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'name',
+                    'surname',
+                    'password1',
+                    'password2',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                ),
+            },
+        ),
+    )
